780a8f7b84e71118443d5b1fe4a05d4d43bc44cf
ramesh pamidi <rameshpamidi45@gmail.com>
2018-03-23 12:14:11 +0530

    modified

--- db-service/src/main/java/com/iconma/DbServiceApplication.java
+++ db-service/src/main/java/com/iconma/DbServiceApplication.java
@@ -8,8 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages={"com.iconma.repository"})
-public class DbServiceApplication {
 
+public class DbServiceApplication {
+    
 	public static void main(String[] args) {
 		SpringApplication.run(DbServiceApplication.class, args);
 	}
